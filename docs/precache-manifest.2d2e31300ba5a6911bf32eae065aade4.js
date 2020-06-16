60116569d8c2372376d9fb10c7a2c584ec0b6eeb
mgracesb <mercysbarcel@gmail.com>
2020-06-16 16:47:20 +0200

    edit json

---
+++ docs/precache-manifest.2d2e31300ba5a6911bf32eae065aade4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14b129c19e153eea91bc7f3349fe0517",
+    "url": "./index.html"
+  },
+  {
+    "revision": "61eddaa772c0ebfe108c",
+    "url": "./static/css/main.d4dac2ca.chunk.css"
+  },
+  {
+    "revision": "679bd26cc2f5384856ff",
+    "url": "./static/js/2.7085bef3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.7085bef3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61eddaa772c0ebfe108c",
+    "url": "./static/js/main.da36639e.chunk.js"
+  },
+  {
+    "revision": "b2230e7f775854a97f51",
+    "url": "./static/js/runtime-main.499832b1.js"
+  }
+]);
